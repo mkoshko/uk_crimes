ac3952ddc3460f83fe58b17b61878137d4ae86a7
Maksim Kashko <maksim_kashko@epam.com>
2020-01-24 19:01:58 +0300

    Annotated as Repository

--- Task2/src/main/java/by/koshko/crimes/dao/StreetDao.java
+++ Task2/src/main/java/by/koshko/crimes/dao/StreetDao.java
@@ -1,11 +1,22 @@
 package by.koshko.crimes.dao;
 
 import by.koshko.crimes.entity.Street;
+import org.codejargon.fluentjdbc.api.query.Query;
+import org.springframework.stereotype.Repository;
 
-public class StreetDao implements Dao<Street> {
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+@Repository
+public class StreetDao extends AbstractDao implements Dao<Street> {
+
+    private static final String INSERT_QUERY = "INSERT INTO street (id, name) VALUES (?, ?);";
 
     @Override
     public void save(Street street) {
-
+        Stream<List<?>> params = Stream.of(Arrays.asList(street.getId(), street.getName()));
+        Query query = fluentJdbc.query();
+        query.batch(INSERT_QUERY).params(params).run();
     }
 }
