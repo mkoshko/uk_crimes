c24611f04f325cebf5e2339e8ee1b8e9c07aa638
Maksim Kashko <maksim_kashko@epam.com>
2020-01-24 18:05:56 +0300

    StreetDao class(empty) added

---
+++ Task2/src/main/java/by/koshko/crimes/dao/StreetDao.java
@@ -0,0 +1,11 @@
+package by.koshko.crimes.dao;
+
+import by.koshko.crimes.entity.Street;
+
+public class StreetDao implements Dao<Street> {
+
+    @Override
+    public void save(Street street) {
+
+    }
+}
