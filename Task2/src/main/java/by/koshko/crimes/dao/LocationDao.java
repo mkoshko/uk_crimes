94741b879d1078b9eff6532e0b9962b263eb12c7
Maksim Kashko <maksim_kashko@epam.com>
2020-01-24 18:05:27 +0300

    LocationDao implementation added

---
+++ Task2/src/main/java/by/koshko/crimes/dao/LocationDao.java
@@ -0,0 +1,26 @@
+package by.koshko.crimes.dao;
+
+import by.koshko.crimes.entity.Location;
+import org.codejargon.fluentjdbc.api.query.Query;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class LocationDao extends AbstractDao implements Dao<Location> {
+
+    private static final String INSERT_QUERY = "INSERT INTO location (street_id, latitude, longitude) VALUES (?, ?, ?);";
+
+    @Override
+    public void save(Location location) {
+        Stream<List<?>> params = Stream.of(Arrays.asList(
+                location.getStreet().getId(),
+                location.getLatitude(),
+                location.getLongitude()
+        ));
+        Query query = fluentJdbc.query();
+        query.batch(INSERT_QUERY)
+                .params(params)
+                .run();
+    }
+}
