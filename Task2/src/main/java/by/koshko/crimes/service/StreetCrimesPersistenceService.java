d0b268e6258e2bfcbab72542cfc46421026eecb8
Maksim Kashko <maksim_kashko@epam.com>
2020-01-26 22:35:22 +0300

    add PersistenceService implementation for street level crime API method

---
+++ Task2/src/main/java/by/koshko/crimes/service/StreetCrimesPersistenceService.java
@@ -0,0 +1,63 @@
+package by.koshko.crimes.service;
+
+import by.koshko.crimes.dao.impl.CrimeDao;
+import by.koshko.crimes.dao.impl.LocationDao;
+import by.koshko.crimes.dao.impl.OutcomeStatusDao;
+import by.koshko.crimes.dao.impl.StreetDao;
+import by.koshko.crimes.entity.Crime;
+import by.koshko.crimes.entity.Location;
+import by.koshko.crimes.entity.OutcomeStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@org.springframework.stereotype.Service
+public class StreetCrimesPersistenceService implements PersistenceService<Crime> {
+
+    private CrimeDao crimeDao;
+    private LocationDao locationDao;
+    private StreetDao streetDao;
+    private OutcomeStatusDao outcomeStatusDao;
+
+    @Autowired
+    public StreetCrimesPersistenceService(CrimeDao crimeDao, LocationDao locationDao,
+                                          StreetDao streetDao, OutcomeStatusDao outcomeStatusDao) {
+        this.crimeDao = crimeDao;
+        this.locationDao = locationDao;
+        this.streetDao = streetDao;
+        this.outcomeStatusDao = outcomeStatusDao;
+    }
+
+    @Override
+    @Transactional
+    public void save(Crime crime) {
+        if (crime != null) {
+            saveStreet(crime);
+            saveLocation(crime.getLocation());
+            saveOutcomeStatus(crime.getOutcome_status());
+            crimeDao.save(crime);
+        }
+    }
+
+    private void saveStreet(Crime crime) {
+        Optional.ofNullable(crime)
+                .map(Crime::getLocation)
+                .map(Location::getStreet)
+                .ifPresent(streetDao::save);
+    }
+
+    private void saveLocation(Location location) {
+        if (location != null) {
+            long id = locationDao.save(location);
+            location.setId(id);
+        }
+    }
+
+    private void saveOutcomeStatus(OutcomeStatus outcomeStatus) {
+        if (outcomeStatus != null) {
+            long id = outcomeStatusDao.save(outcomeStatus);
+            outcomeStatus.setId(id);
+        }
+    }
+}
