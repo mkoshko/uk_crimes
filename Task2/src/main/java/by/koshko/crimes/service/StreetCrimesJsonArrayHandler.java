332b445bdf9d078fe2da7f98e6b42e9f40b4a141
Maksim Kashko <maksim_kashko@epam.com>
2020-01-26 22:35:03 +0300

    add JsonArrayHandler implementation for street level crime API method

---
+++ Task2/src/main/java/by/koshko/crimes/service/StreetCrimesJsonArrayHandler.java
@@ -0,0 +1,42 @@
+package by.koshko.crimes.service;
+
+import by.koshko.crimes.service.mapper.CrimeMapper;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Stream;
+
+@Service
+public class StreetCrimesJsonArrayHandler implements JsonArrayHandler {
+
+    private JsonArrayStreamSupport jsonArrayStreamSupport;
+    private CrimeMapper crimeMapper;
+    private StreetCrimesPersistenceService persistenceService;
+
+    @Autowired
+    public StreetCrimesJsonArrayHandler(JsonArrayStreamSupport converter,
+                                        CrimeMapper crimeMapper,
+                                        StreetCrimesPersistenceService persistenceService) {
+        this.jsonArrayStreamSupport = converter;
+        this.crimeMapper = crimeMapper;
+        this.persistenceService = persistenceService;
+    }
+
+    @Override
+    public void process(String jsonArrayString) throws ServiceException {
+        JSONArray jsonArray = createJsonArray(jsonArrayString);
+        Stream<JSONObject> jsonObjectParallelStream = jsonArrayStreamSupport.toParallelStream(jsonArray);
+        jsonObjectParallelStream.map(crimeMapper::map).forEach(persistenceService::save);
+    }
+
+    private JSONArray createJsonArray(String json) throws ServiceException {
+        try {
+            return new JSONArray(json);
+        } catch (JSONException e) {
+            throw new ServiceException("Cannot process json array. Invalid syntax.", e);
+        }
+    }
+}
