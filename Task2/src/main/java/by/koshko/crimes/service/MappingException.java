bdbb02c13e5cfd6392c9c656fea30a8beafae7f3
Maksim Kashko <maksim_kashko@epam.com>
2020-01-27 13:04:36 +0300

    Generic exception for mappers

---
+++ Task2/src/main/java/by/koshko/crimes/service/MappingException.java
@@ -0,0 +1,13 @@
+package by.koshko.crimes.service;
+
+public class MappingException extends Exception {
+
+
+    public MappingException(String message) {
+        super(message);
+    }
+
+    public MappingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
