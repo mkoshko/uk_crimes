1b8e6a28fb2214997928e07b462ffd0257a2586c
Maksim Kashko <maksim_kashko@epam.com>
2020-01-28 23:35:53 +0300

    implement execute method

--- Task2/src/main/java/by/koshko/crimes/service/ApplicationExecutor.java
+++ Task2/src/main/java/by/koshko/crimes/service/ApplicationExecutor.java
@@ -1,8 +1,14 @@
 package by.koshko.crimes.service;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class ApplicationExecutor {
 
-    public void execute(String file, String api, String dateFrom, String dateTo) {
+    private AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(RootConfig.class);
 
+    public void execute(String file, String api, String dateFrom, String dateTo) {
+        ExecutorServiceBuilder builder = (ExecutorServiceBuilder) context.getBean(api);
+        ExecutorService executorService = builder.build();
+        executorService.execute(file, dateFrom, dateTo);
     }
 }
