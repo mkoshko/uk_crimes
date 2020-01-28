94a194f99b43716815ccb379fddaef433264a01b
Maksim Kashko <maksim_kashko@epam.com>
2020-01-28 23:36:36 +0300

    add generics

--- Task2/src/main/java/by/koshko/crimes/service/ExecutorService.java
+++ Task2/src/main/java/by/koshko/crimes/service/ExecutorService.java
@@ -1,6 +1,47 @@
 package by.koshko.crimes.service;
 
-public interface ExecutorService {
+import by.koshko.crimes.dao.CsvFileReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-    void execute(String file, String dateFrom, String dateTo);
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ExecutorService<T, R> {
+
+    private Logger logger = LoggerFactory.getLogger(ExecutorService.class);
+    private RequestDataMapper<R> dataMapper;
+    private HttpRequestService<R> requestService;
+    private JsonArrayHandler<T> jsonArrayHandler;
+
+    public ExecutorService(RequestDataMapper<R> dataMapper,
+                           HttpRequestService<R> requestService,
+                           JsonToObjectMapper<T> objectMapper,
+                           PersistenceService<T> persistenceService) {
+        this.dataMapper = dataMapper;
+        this.requestService = requestService;
+        jsonArrayHandler = new JsonArrayHandler<>(objectMapper, persistenceService);
+    }
+
+    public void execute(String file, String dateFrom, String dateTo) {
+        try {
+            List<R> data = readDataFromFile(file);
+            data.parallelStream().forEach(point -> {
+                try {
+                    String jsonResponse = requestService.sendRequest(point);
+                    jsonArrayHandler.process(jsonResponse);
+                } catch (ServiceException e) {
+                    logger.error(e.getMessage());
+                }
+            });
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    private List<R> readDataFromFile(String file) throws IOException {
+        Stream<String> data = CsvFileReader.getLinesAsStream(file);
+        return dataMapper.map(data);
+    }
 }
--- Task2/src/main/java/by/koshko/crimes/service/JsonArrayHandler.java
+++
@@ -1,6 +0,0 @@
-package by.koshko.crimes.service;
-
-public interface JsonArrayHandler {
-
-    void process(String jsonArray) throws ServiceException;
-}
