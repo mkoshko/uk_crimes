6f1e78773c93279e221b5ab3fd3a38fe9595a2f9
Maksim Kashko <maksim_kashko@epam.com>
2020-01-28 19:50:09 +0300

    add abstract class with most of implemented methods from HttpRequestService interface

---
+++ Task2/src/main/java/by/koshko/crimes/service/AbstractHttpRequestService.java
@@ -0,0 +1,44 @@
+package by.koshko.crimes.service;
+
+import by.koshko.crimes.entity.Point;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public abstract class AbstractHttpRequestService implements HttpRequestService<Point> {
+
+    private Logger logger = LoggerFactory.getLogger(StreetCrimesHttpRequestService.class);
+    private String requestUrl;
+
+    public AbstractHttpRequestService(String requestUrl) {
+        this.requestUrl = requestUrl;
+    }
+
+    @Override
+    public String sendRequest(Point point, String... additionalParams) throws ServiceException {
+        logger.info("Requesting information for " + point.getName() + ".");
+        try {
+            URL url = new URL(this.requestUrl + buildRequestParameters(point, additionalParams));
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoOutput(true);
+            return getResponse(connection);
+        } catch (IOException e) {
+            throw new ServiceException("Cannot send request.", e);
+        }
+    }
+
+    private String getResponse(HttpURLConnection connection) throws IOException {
+        try (InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream())) {
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            return reader.readLine();
+        }
+    }
+
+    abstract String buildRequestParameters(Point point, String... additionalParams);
+}
