050c45fac118f23a5221712d5e1a25b1675f12cf
Maksim Kashko <maksim_kashko@epam.com>
2020-01-31 21:54:06 +0300

    refactoring

--- Task2/src/main/java/by/koshko/crimes/service/ExecutionService.java
+++ Task2/src/main/java/by/koshko/crimes/service/ExecutionService.java
@@ -1,31 +1,30 @@
 package by.koshko.crimes.service;
 
 import by.koshko.crimes.dao.CsvFileReader;
-import by.koshko.crimes.entity.FailedRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.time.format.DateTimeParseException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
 public class ExecutionService<T, R> {
 
-    ExecutorService executorService = Executors.newFixedThreadPool(30);
-    ExecutorService requestExecutor = Executors.newFixedThreadPool(30);
+    ExecutorService requestProcessor = Executors.newFixedThreadPool(15);
+    ExecutorService persistProcessor = Executors.newFixedThreadPool(15);
     private Logger logger = LoggerFactory.getLogger(ExecutionService.class);
     private RequestDataMapper<R> dataMapper;
     private HttpRequestService<R> requestService;
+    private BlockingQueue<Future<String>> tasks = new LinkedBlockingQueue<>(15);
+    private BlockingQueue<Future<String>> bufferedTasks = new LinkedBlockingQueue<>();
     private JsonArrayHandler<T> jsonArrayHandler;
-    private List<FailedRequest<R>> failedRequests = new ArrayList<>();
-    private AtomicInteger numberOfRequests = new AtomicInteger(0);
-    private final int API_CALL_LIMIT = 15;
-    private long startTime = System.currentTimeMillis();
 
     public ExecutionService(RequestDataMapper<R> dataMapper,
                             HttpRequestService<R> requestService,
@@ -37,65 +36,56 @@ public class ExecutionService<T, R> {
     }
 
     public void execute(String file, String startDate, String endDate) throws ApplicationException {
-        DateRange dateRange = createDateRange(startDate, endDate);
-        List<R> data = readDataFromFile(file);
+        DateRange dateRange = DateRange.build(startDate, endDate);
+        List<R> data = readAndMapDataFromFile(file);
         execute0(data, dateRange);
     }
 
     private void execute0(List<R> data, DateRange dateRange) {
-        data.forEach(point -> dateRange.stream().forEach(date -> {
-            checkTimeAndReset();
-            processRequest(point, date);
+        requestProcessor.execute(() -> {
+            while (!(persistProcessor.isTerminated() && tasks.isEmpty())) {
+                try {
+                    if (tasks.isEmpty()) {
+                        TimeUnit.MILLISECONDS.sleep(100);
+                    }
+                    Future<String> task = tasks.take();
+                    while (!task.isDone()) {
+                        TimeUnit.MILLISECONDS.sleep(100);
+                    }
+                    persistProcessor.execute(() -> {
+                        try {
+                            logger.info("Processing response...");
+                            jsonArrayHandler.process(task.get());
+                        } catch (ServiceException e) {
+                            logger.error("Cannot process the response. {}", e.getMessage());
+                        } catch (InterruptedException | ExecutionException e1) {
+                            logger.error(e1.getMessage());
+                        }
+                    });
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+        dateRange.forEach(date -> data.forEach(point -> {
+            Future<String> task = processRequest(point, date);
+            try {
+                tasks.put(task);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
         }));
     }
 
-    private void processRequest(R point ,String date) {
-        executorService.execute(() -> {
-            try {
-                while (isApiCallLimitExceeded()) {
-                    TimeUnit.MILLISECONDS.sleep(50);
-                }
-                numberOfRequests.incrementAndGet();
-                Future<String> response = requestExecutor.submit(() -> requestService.sendRequest(point, date));
-                while (!response.isDone()) {
-                    TimeUnit.MILLISECONDS.sleep(100);
-                }
-                numberOfRequests.decrementAndGet();
-                jsonArrayHandler.process(response.get());
-            } catch (ServiceException e) {
-                failedRequests.add(new FailedRequest<>(point, date));
-                logger.error("Cannot process the request. {}", e.getMessage());
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            } catch (ExecutionException e) {
-                logger.error(e.getMessage());
-            }
-        });
+    private void throttle() {
+
     }
 
-    private void checkTimeAndReset() {
-        if (System.currentTimeMillis() - startTime > TimeUnit.SECONDS.toMillis(1)) {
-            numberOfRequests.set(0);
-            startTime = System.currentTimeMillis();
-        }
+    private Future<String> processRequest(R point, String date) {
+        return requestProcessor.submit(() -> requestService.sendRequest(point, date));
     }
 
-    private boolean isApiCallLimitExceeded() {
-        return numberOfRequests.get() > API_CALL_LIMIT;
-    }
-
-    private DateRange createDateRange(String startDate, String endDate) throws ApplicationException {
-        try {
-            if (Objects.nonNull(startDate) || Objects.nonNull(endDate)) {
-                return new DateRange(startDate, endDate);
-            }
-        } catch (DateTimeParseException e) {
-            throw new ApplicationException("Invalid date format.");
-        }
-        throw new ApplicationException("Invalid date range.");
-    }
-
-    private List<R> readDataFromFile(String file) throws ApplicationException {
+    private List<R> readAndMapDataFromFile(String file) throws ApplicationException {
         try {
             Stream<String> data = CsvFileReader.getLinesAsStream(file);
             return dataMapper.map(data);
