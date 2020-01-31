f3348eb8cf80e2ec256aa3e25a158628f1ebbe9c
Maksim Kashko <maksim_kashko@epam.com>
2020-01-31 13:01:19 +0300

    refactoring

--- Task2/src/main/java/by/koshko/crimes/service/ExecutionService.java
+++ Task2/src/main/java/by/koshko/crimes/service/ExecutionService.java
@@ -1,24 +1,31 @@
 package by.koshko.crimes.service;
 
 import by.koshko.crimes.dao.CsvFileReader;
+import by.koshko.crimes.entity.FailedRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 public class ExecutionService<T, R> {
 
-    ExecutorService executorService = Executors.newFixedThreadPool(15);
+    ExecutorService executorService = Executors.newCachedThreadPool();
+    ExecutorService requestExecutor = Executors.newCachedThreadPool();
     private Logger logger = LoggerFactory.getLogger(ExecutionService.class);
     private RequestDataMapper<R> dataMapper;
     private HttpRequestService<R> requestService;
     private JsonArrayHandler<T> jsonArrayHandler;
-    private ConcurrentLinkedQueue<Future<String>> futures = new ConcurrentLinkedQueue<>();
+    private List<FailedRequest<R>> failedRequests = new ArrayList<>();
+    private AtomicInteger numberOfRequests = new AtomicInteger(0);
+    private final int API_CALL_LIMIT = 15;
+    private long startTime = System.currentTimeMillis();
 
     public ExecutionService(RequestDataMapper<R> dataMapper,
                             HttpRequestService<R> requestService,
@@ -36,14 +43,48 @@ public class ExecutionService<T, R> {
     }
 
     private void execute0(List<R> data, DateRange dateRange) {
-        data.parallelStream().forEach(point -> dateRange.stream().forEach(date -> {
+
+        data.forEach(point -> dateRange.stream().forEach(date -> {
+            checkTimeAndReset();
+            processRequest(point, date);
+        }));
+    }
+
+    private void processRequest(R point ,String date) {
+        executorService.execute(() -> {
             try {
-                String jsonResponse = requestService.sendRequest(point, date);
-                jsonArrayHandler.process(jsonResponse);
+                while (isApiCallLimitExceeded()) {
+                    logger.info("Number of requests is exceeded. Waiting...");
+                    TimeUnit.MILLISECONDS.sleep(50);
+                }
+                logger.info("Number of request: {}.", numberOfRequests.incrementAndGet());
+                Future<String> response = requestExecutor.submit(() -> requestService.sendRequest(point, date));
+                while (!response.isDone()) {
+                    TimeUnit.MILLISECONDS.sleep(100);
+                }
+                logger.info("Request is complete. Number of request: {}.", numberOfRequests.decrementAndGet());
+                jsonArrayHandler.process(response.get());
             } catch (ServiceException e) {
+                failedRequests.add(new FailedRequest<>(point, date));
+                logger.error("Cannot process the request. {}", e.getMessage());
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            } catch (ExecutionException e) {
                 logger.error(e.getMessage());
             }
-        }));
+        });
+    }
+
+    private void checkTimeAndReset() {
+        if (System.currentTimeMillis() - startTime > TimeUnit.SECONDS.toMillis(1)) {
+            logger.info("Reset number of requests.");
+            numberOfRequests.set(0);
+            startTime = System.currentTimeMillis();
+        }
+    }
+
+    private boolean isApiCallLimitExceeded() {
+        return numberOfRequests.get() > API_CALL_LIMIT;
     }
 
     private DateRange createDateRange(String startDate, String endDate) throws ApplicationException {
