c9ce32147c84a581c5c70fdcdcc3cc8be7150921
Maksim Kashko <maksim_kashko@epam.com>
2020-02-10 23:48:44 +0300

    OutcomeObjectDao implementation

---
+++ Task3/src/main/java/by/koshko/crimes/dao/impl/OutcomeObjectDaoImpl.java
@@ -0,0 +1,52 @@
+package by.koshko.crimes.dao.impl;
+
+import by.koshko.crimes.dao.OutcomeObjectDao;
+import by.koshko.crimes.entity.OutcomeObject;
+import org.codejargon.fluentjdbc.api.FluentJdbc;
+import org.codejargon.fluentjdbc.api.mapper.Mappers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class OutcomeObjectDaoImpl implements OutcomeObjectDao {
+
+    private static final String INSERT_QUERY
+            = "INSERT INTO outcome_object (identity, name) VALUES (?, ?) ON CONFLICT DO NOTHING RETURNING id";
+    private static final String GET_BY_ID_QUERY
+            = "SELECT id FROM outcome_object WHERE identity=?;";
+    private FluentJdbc fluentJdbc;
+
+    @Autowired
+    public OutcomeObjectDaoImpl(FluentJdbc fluentJdbc) {
+        this.fluentJdbc = fluentJdbc;
+    }
+
+    @Override
+    public long save(OutcomeObject outcomeObject) {
+        return fluentJdbc.query()
+                .update(INSERT_QUERY)
+                .params(paramsToList(outcomeObject))
+                .runFetchGenKeys(Mappers.singleLong())
+                .firstKey()
+                .orElse(0L);
+    }
+
+    private List<?> paramsToList(OutcomeObject entity) {
+        List<Object> params = new ArrayList<>();
+        params.add(entity.getId());
+        params.add(entity.getName());
+        return params;
+    }
+
+    @Override
+    public long findId(String identity) {
+        return fluentJdbc.query()
+                .select(GET_BY_ID_QUERY)
+                .params(identity)
+                .firstResult(rs -> rs.getLong(1))
+                .orElse(0L);
+    }
+}
