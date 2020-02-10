b16b750adaeb107d02e544ebffb8d0f9e273dd1c
Maksim Kashko <maksim_kashko@epam.com>
2020-02-10 23:47:15 +0300

    add persistence service

---
+++ Task3/src/main/java/by/koshko/crimes/service/persistence/LocationPersistenceService.java
@@ -0,0 +1,33 @@
+package by.koshko.crimes.service.persistence;
+
+import by.koshko.crimes.dao.impl.LocationDao;
+import by.koshko.crimes.dao.impl.StreetDao;
+import by.koshko.crimes.entity.Location;
+import by.koshko.crimes.service.PersistenceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class LocationPersistenceService implements PersistenceService<Location> {
+
+    private LocationDao locationDao;
+    private StreetDao streetDao;
+
+    @Autowired
+    public LocationPersistenceService(LocationDao locationDao, StreetDao streetDao) {
+        this.locationDao = locationDao;
+        this.streetDao = streetDao;
+    }
+
+    @Override
+    public void save(Location location) {
+        Optional.ofNullable(location)
+                .map(Location::getStreet)
+                .ifPresent(street -> {
+                    streetDao.save(street);
+                    locationDao.save(location);
+                });
+    }
+}
---
+++ Task3/src/main/java/by/koshko/crimes/service/persistence/OutcomeObjectPersistenceService.java
@@ -0,0 +1,33 @@
+package by.koshko.crimes.service.persistence;
+
+import by.koshko.crimes.dao.OutcomeObjectDao;
+import by.koshko.crimes.entity.OutcomeObject;
+import by.koshko.crimes.service.PersistenceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OutcomeObjectPersistenceService implements PersistenceService<OutcomeObject> {
+
+    private OutcomeObjectDao outcomeObjectDao;
+
+    @Autowired
+    public OutcomeObjectPersistenceService(OutcomeObjectDao outcomeObjectDao) {
+        this.outcomeObjectDao = outcomeObjectDao;
+    }
+
+    @Override
+    public void save(OutcomeObject outcomeObject) {
+        if (outcomeObject != null) {
+            saveOrGetExisting(outcomeObject);
+        }
+    }
+
+    private void saveOrGetExisting(OutcomeObject outcomeObject) {
+        long id = outcomeObjectDao.save(outcomeObject);
+        if (id == 0) {
+            id = outcomeObjectDao.findId(outcomeObject.getIdentity());
+        }
+        outcomeObject.setId(id);
+    }
+}
