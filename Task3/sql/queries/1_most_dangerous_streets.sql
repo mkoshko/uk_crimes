7fcf4af6bd7f6a3030d565e8474abc99337c9cd0
Maksim Kashko <maksim_kashko@epam.com>
2020-02-11 20:37:19 +0300

    add date range

--- Task3/sql/queries/1_most_dangerous_streets.sql
+++ Task3/sql/queries/1_most_dangerous_streets.sql
@@ -1,9 +1,10 @@
 SELECT
-       street_id AS "StreetID",
-       name AS "StreetName",
-       count(*) AS "CrimesNumber"
+    street_id AS "StreetID",
+    name AS "StreetName",
+    count(*) AS "CrimesNumber"
 FROM crime
-INNER JOIN location ON crime.location_id = location.street_id
-INNER JOIN street street_table ON location.street_id = street_table.id
+         INNER JOIN location ON crime.location_id = location.street_id
+         INNER JOIN street street_table ON location.street_id = street_table.id
+WHERE month >= '2019-01' AND month <= '2019-05'
 GROUP BY street_id, name
 ORDER BY count(*) DESC;
