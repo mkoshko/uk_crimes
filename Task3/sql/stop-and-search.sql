a9f4d91438d649c9b8a8dfd2c9bcf4815fd83947
Maksim Kashko <maksim_kashko@epam.com>
2020-02-11 18:18:32 +0300

    change varchar capacity

--- Task3/sql/stop-and-search.sql
+++ Task3/sql/stop-and-search.sql
@@ -10,22 +10,25 @@ create table outcome_object
 create table stop_and_search
 (
     id serial8,
-    type varchar(30),
+    type varchar(50),
     involved_person boolean,
     datetime timestamp,
     operation boolean,
     operation_name varchar(100),
     location_id bigint,
-    gender varchar(6),
-    age_range varchar(6),
-    self_defined_ethnicity varchar(50),
-    officer_defined_ethnicity varchar(50),
+    gender varchar(10),
+    age_range varchar(10),
+    self_defined_ethnicity varchar(100),
+    officer_defined_ethnicity varchar(100),
     legislation varchar(100),
-    object_of_search varchar(50),
-    outcome varchar(50),
+    object_of_search varchar(100),
+    outcome varchar(100),
     outcome_object_id integer,
     outcome_linked_to_object_of_search boolean,
     removal_of_more_than_outer_clothing boolean,
     constraint FK_stop_and_search_location foreign key (location_id) references location(street_id),
     constraint FK_stop_and_search_outcome_object foreign key (outcome_object_id) references outcome_object(id)
 );
+
+grant select, update, insert, delete on all tables in schema public to crime_app;
+grant usage, select on all sequences in schema public to crime_app;
