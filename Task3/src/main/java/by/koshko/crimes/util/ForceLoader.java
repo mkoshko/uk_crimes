1faaa6fa7002c769d49b435b5ff0783bf7e910dd
Maksim Kashko <maksim_kashko@epam.com>
2020-02-11 19:44:31 +0300

    change return type

--- Task3/src/main/java/by/koshko/crimes/util/ForceLoader.java
+++ Task3/src/main/java/by/koshko/crimes/util/ForceLoader.java
@@ -2,7 +2,7 @@ package by.koshko.crimes.util;
 
 import by.koshko.crimes.entity.Force;
 import by.koshko.crimes.service.exception.ServiceException;
-import by.koshko.crimes.service.impl.ForcesHttpRequestService;
+import by.koshko.crimes.service.impl.AvailableForcesHttpRequest;
 import by.koshko.crimes.service.jsonutil.JsonArrayStreamSupport;
 import by.koshko.crimes.service.mapper.ForceMapper;
 import org.json.JSONArray;
@@ -13,14 +13,13 @@ import java.util.stream.Collectors;
 public class ForceLoader {
 
     private String url = "https://data.police.uk/api/forces";
-    private ForcesHttpRequestService requestService = new ForcesHttpRequestService(url);
+    private AvailableForcesHttpRequest requestService = new AvailableForcesHttpRequest(url);
     private ForceMapper forceMapper = new ForceMapper();
 
-    public List<String> getForces() throws ServiceException {
+    public List<Force> getForces() throws ServiceException {
         String response = requestService.sendRequest(null);
         return JsonArrayStreamSupport.toStream(new JSONArray(response))
                 .map(forceMapper::map)
-                .map(Force::getId)
                 .collect(Collectors.toList());
     }
 }
