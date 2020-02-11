4fa9c05629ce0adf370fcd88f27bceed824a8acc
Maksim Kashko <maksim_kashko@epam.com>
2020-02-11 18:23:12 +0300

    Abstract class for easy api method implementation which uses Point as request parameter

--- Task3/src/main/java/by/koshko/crimes/api/AbstractPointBasedApiModule.java
+++ Task3/src/main/java/by/koshko/crimes/api/AbstractPointBasedApiModule.java
@@ -1,53 +1,46 @@
 package by.koshko.crimes.api;
 
-import by.koshko.crimes.entity.Crime;
 import by.koshko.crimes.entity.Point;
-import by.koshko.crimes.exec.ApplicationAPIModule;
 import by.koshko.crimes.service.ExecutionService;
+import by.koshko.crimes.service.JsonToObjectMapper;
 import by.koshko.crimes.service.PersistenceService;
 import by.koshko.crimes.service.exception.ApplicationException;
 import by.koshko.crimes.service.impl.PointHttpRequestService;
-import by.koshko.crimes.service.mapper.CrimeMapper;
 import by.koshko.crimes.service.mapper.PointMapper;
 import by.koshko.crimes.util.CommandLineParameters;
 import by.koshko.crimes.util.CsvFileReader;
 import by.koshko.crimes.util.DateRange;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Properties;
 import java.util.stream.Stream;
 
-@Component("street-level-crimes")
-public class StreetLevelCrimesAPIModule implements ApplicationAPIModule {
+public abstract class AbstractPointBasedApiModule<T> implements ApplicationApiModule {
 
-    private static final String REQUEST_URL = "https://data.police.uk/api/crimes-street/all-crime";
-    private static Logger logger = LoggerFactory.getLogger(StreetLevelCrimesAPIModule.class);
+    private Logger logger = LoggerFactory.getLogger(getClass());
     private PointMapper pointMapper;
     private PointHttpRequestService requestService;
-    private CrimeMapper crimeMapper;
-    private PersistenceService<Crime> persistenceService;
+    private JsonToObjectMapper<T> jsonToObjectMapper;
+    private PersistenceService<T> persistenceService;
 
-    @Autowired
-    public StreetLevelCrimesAPIModule(PointMapper pointMapper,
-                                      CrimeMapper crimeMapper,
-                                      PersistenceService<Crime> persistenceService) {
+    public AbstractPointBasedApiModule(PointMapper pointMapper,
+                                       JsonToObjectMapper<T> jsonToObjectMapper,
+                                       PersistenceService<T> persistenceService,
+                                       PointHttpRequestService requestService) {
         this.pointMapper = pointMapper;
-        this.requestService = new PointHttpRequestService(REQUEST_URL);
-        this.crimeMapper = crimeMapper;
+        this.requestService = requestService;
+        this.jsonToObjectMapper = jsonToObjectMapper;
         this.persistenceService = persistenceService;
     }
 
     @Override
     public void run(Properties parameters) {
         try {
-            ExecutionService<Crime, Point> executionService
-                    = new ExecutionService<>(requestService, crimeMapper, persistenceService);
+            ExecutionService<T, Point> executionService
+                    = new ExecutionService<>(requestService, jsonToObjectMapper, persistenceService);
             DateRange dateRange = DateRange.build(
                     parameters.getProperty(CommandLineParameters.FROM_OPTION),
                     parameters.getProperty(CommandLineParameters.TO_OPTION));
