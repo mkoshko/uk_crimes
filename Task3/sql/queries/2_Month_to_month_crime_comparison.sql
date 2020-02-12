c2d6c691c2e7303e54d8669ede3b5fc9253989d2
Maksim Kashko <maksim_kashko@epam.com>
2020-02-12 19:53:46 +0300

    Crimes with specified outcome status sql query

--- Task3/sql/queries/2_Month_to_month_crime_comparison.sql
+++ Task3/sql/queries/2_Month_to_month_crime_comparison.sql
@@ -7,4 +7,5 @@ COUNT(*) - LAG(COUNT(*)) OVER(PARTITION BY category) AS "Delta",
 ROUND((((COUNT(*) - LAG(COUNT(*)) OVER(PARTITION BY category)) / LAG(COUNT(*)) OVER(PARTITION BY category)::numeric) * 100), 2) AS "GrowthRate"
 FROM crime
 WHERE month >= '2019-01' AND month <= '2019-05'
-GROUP BY category, month;
+GROUP BY category, month
+ORDER BY category, month;
