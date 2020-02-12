e4458a19acd3f6adfce98ab7bff4abb7f56aa55b
Maksim Kashko <maksim_kashko@epam.com>
2020-02-12 11:53:17 +0300

    refactoring

--- Task3/sql/queries/2_Month_to_month_crime_comparison.sql
+++ Task3/sql/queries/2_Month_to_month_crime_comparison.sql
@@ -1,18 +1,10 @@
-WITH crimes AS (
-    SELECT
-        category AS CrimeCategory,
-        month AS Month,
-        COUNT(*) AS currentMonth
-    FROM crime
-    WHERE month >= '2019-01' AND month <= '2019-05'
-    GROUP BY category, month
-)
 SELECT
-    CrimeCategory,
-    Month,
-    currentMonth,
-    LAG(crimes.currentMonth) OVER (PARTITION BY CrimeCategory ORDER BY Month) AS "previousMonth",
-    currentMonth - LAG(crimes.currentMonth) OVER (PARTITION BY CrimeCategory ORDER BY Month) AS "Delta",
-    ROUND((((currentMonth - LAG(crimes.currentMonth) OVER (PARTITION BY CrimeCategory ORDER BY Month))
-         / LAG(crimes.currentMonth) OVER (PARTITION BY CrimeCategory ORDER BY Month)::numeric) * 100), 2) AS "Growth"
-FROM crimes
+category AS "CrimeCategory",
+"month" AS "Month",
+LAG(COUNT(*)) OVER(PARTITION BY category) AS "PreviousMonth",
+COUNT(*) AS "CurrentMonth",
+COUNT(*) - LAG(COUNT(*)) OVER(PARTITION BY category) AS "Delta",
+ROUND((((COUNT(*) - LAG(COUNT(*)) OVER(PARTITION BY category)) / LAG(COUNT(*)) OVER(PARTITION BY category)::numeric) * 100), 2) AS "GrowthRate"
+FROM crime
+WHERE month >= '2019-01' AND month <= '2019-05'
+GROUP BY category, month;
