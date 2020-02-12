08f02dbf64822ea8355bef5e0850a078bbd17492
Maksim Kashko <maksim_kashko@epam.com>
2020-02-12 20:39:11 +0300

    formatted

--- Task3/sql/queries/2_Month_to_month_crime_comparison.sql
+++ Task3/sql/queries/2_Month_to_month_crime_comparison.sql
@@ -1,11 +1,13 @@
 SELECT
-category AS "CrimeCategory",
-"month" AS "Month",
-LAG(COUNT(*)) OVER(PARTITION BY category) AS "PreviousMonth",
-COUNT(*) AS "CurrentMonth",
-COUNT(*) - LAG(COUNT(*)) OVER(PARTITION BY category) AS "Delta",
-ROUND((((COUNT(*) - LAG(COUNT(*)) OVER(PARTITION BY category)) / LAG(COUNT(*)) OVER(PARTITION BY category)::numeric) * 100), 2) AS "GrowthRate"
+    category AS "CrimeCategory",
+    "month" AS "Month",
+    LAG(COUNT(*)) OVER(PARTITION BY category) AS "PreviousMonth",
+    COUNT(*) AS "CurrentMonth",
+    COUNT(*) - LAG(COUNT(*)) OVER(PARTITION BY category) AS "Delta",
+    ROUND((((COUNT(*) - LAG(COUNT(*)) OVER(PARTITION BY category)) / LAG(COUNT(*)) OVER(PARTITION BY category)::numeric) * 100), 2) AS "GrowthRate"
 FROM crime
-WHERE month >= '2019-01' AND month <= '2019-05'
+WHERE
+    month >= '2019-01'
+    AND month <= '2019-05'
 GROUP BY category, month
 ORDER BY category, month;
--- Task3/sql/queries/3_Crimes_with_specified_outcome_status.sql
+++ Task3/sql/queries/3_Crimes_with_specified_outcome_status.sql
@@ -5,9 +5,10 @@ SELECT DISTINCT
     COUNT(*) OVER (PARTITION BY s.id ORDER BY s.id) AS "CrimesNumber",
     ROUND(((COUNT(*) OVER (PARTITION BY s.id ORDER BY s.id) / COUNT(*) OVER()::numeric) * 100), 2) AS "Persentage"
 FROM crime
-         INNER JOIN outcome_status os ON outcome_status_id = os.id
-         INNER JOIN category_name cn ON os.category_name_id = cn.id
-         INNER JOIN street s ON s.id = location_id
-WHERE month >= '2019-01'
-  AND month <='2019-05'
-  AND cn.category_name='Defendant found not guilty'
+INNER JOIN outcome_status os ON outcome_status_id = os.id
+INNER JOIN category_name cn ON os.category_name_id = cn.id
+INNER JOIN street s ON s.id = location_id
+WHERE
+    month >= '2019-01'
+    AND month <='2019-05'
+    AND cn.category_name='Defendant found not guilty'
