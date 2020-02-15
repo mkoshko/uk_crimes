26ecacf42e157957cdbf788435e18fc72fd23424
Maksim Kashko <maksim_kashko@epam.com>
2020-02-15 21:34:15 +0300

    fix typo in word

--- Task3/sql/queries/3.sql
+++ Task3/sql/queries/3.sql
@@ -3,7 +3,7 @@ SELECT DISTINCT
     s.name,
     cn.category_name,
     COUNT(*) OVER (PARTITION BY s.id ORDER BY s.id) AS "CrimesNumber",
-    ROUND(((COUNT(*) OVER (PARTITION BY s.id ORDER BY s.id) / COUNT(*) OVER()::numeric) * 100), 2) AS "Persentage"
+    ROUND(((COUNT(*) OVER (PARTITION BY s.id ORDER BY s.id) / COUNT(*) OVER()::numeric) * 100), 2) AS "Percentage"
 FROM crime
 INNER JOIN outcome_status os ON outcome_status_id = os.id
 INNER JOIN category_name cn ON os.category_name_id = cn.id
