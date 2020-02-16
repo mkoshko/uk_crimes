a64db5b0486f1e135fdc30f39468cfbbf86e510b
Maksim Kashko <maksim_kashko@epam.com>
2020-02-16 13:20:06 +0300

    script improvements

--- Task3/script/deployment.sh
+++ Task3/script/deployment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-quiet="-q"
+quiet=" -q "
 dbuser="postgres"
 dbname="crimebox"
 tb1="crime"
@@ -11,16 +11,25 @@ tb6="stop_and_search"
 tb7="outcome_object"
 project_folder=
 rows=0
+anazyleQuery=0
+api=("" "street-level-crimes" "stop-and-search-by-area" "stop-and-search-by-force")
+selectedApi=0
 
 
 help() {
   echo "Usage: deployment.sh [OPTION]..."
   echo "Options:"
-  echo "  -f <project folder>,   path to the project."
-  echo "  -B,                    build maven project from specified path."
-  echo "  -D,                    drop tables data."
-  echo "  -Q <index>,            index of a query."
-  echo "  -v,                    display non-script output."
+  echo "  -f <project folder>,   Specify path to the project."
+  echo "  -b,                    Build project with maven. Make sure you specify path to the project through -f option."
+  echo "  -d,                    Drop tables data."
+  echo "  -w <index>             Index of api method to download data."
+  echo "                            -1   street level crimes"
+  echo "                            -2   stop and search by area"
+  echo "                            -3   stop and search by force"
+  echo "                            -all download from all available api methods"
+  echo "  -q <index>,            Index of a query (1-6)."
+  echo "  -r <number>,           Number of rows to output. Default value is 0."
+  echo "  -v,                    Display non-script output."
 }
 
 check_postgres() {
@@ -28,27 +37,21 @@ check_postgres() {
   if [[ -z "$status" ]]; then
     service postgresql-10 start
     sleep 10
+  else
+    echo "Postgres service is already running."
   fi
-}
-
-clear_database() {
-  psql -U "$dbuser" -c \\dn
 }
 
 create_tables() {
   echo "Creating tables..."
-  psql -U "$dbuser" "$dbname" -q -f "$project_folder/sql/create_schema_script.sql"
-  psql -U "$dbuser" "$dbname" -q -f "$project_folder/sql/stop-and-search.sql"
-  if [[ "$?" -eq 0 ]]; then
-    echo "Done."
-  else
-    echo "Tables wasn't created."
-  fi
+  psql"$quiet"-U "$dbuser" "$dbname" -q -f "$project_folder/sql/create_schema_script.sql"
+  psql"$quiet"-U "$dbuser" "$dbname" -q -f "$project_folder/sql/stop-and-search.sql"
+  echo "Done."
 }
 
 create_tables_if_not_exists() {
-  local tables=$(psql -U "$dbuser" "$dbname" -c \\dt 2>&1)
-  if [[ "$tables" == "Did not find any relations." ]]; then
+  local tables=$(psql -U "$dbuser" "$dbname" -c \\dt)
+  if [[ -z "$tables" ]]; then
     create_tables
   else
     local numOfTables=$(echo "$tables" | grep -e "$tb1" -e "$tb2" -e "$tb3" -e "$tb4" -e "$tb5" -e "$tb6" -e "$tb7" -c)
@@ -61,7 +64,7 @@ create_tables_if_not_exists() {
 }
 
 query_to_database() {
-  psql -U "$dbuser" "$dbname" -c "$1"
+  psql"$quiet"-U "$dbuser" "$dbname" -c "$1"
 }
 
 drop_table_data() {
@@ -76,51 +79,62 @@ drop_table_data() {
 
 build_project() {
   echo "Building project..."
-  echo "$1"
-  mvn -f "$1" clean package
+  mvn"$quiet"-f "$project_folder" clean package
   echo "Done"
 }
 
 run_application() {
-  java -jar "$project_folder/target/crime.jar" -Dapi=street-level-crimes -Dfile="$project_folder/data/LondonStations.csv" -Dfrom=2019-01 -Dto=2019-05
-  java -jar "$project_folder/target/crime.jar" -Dapi=stop-and-search-by-area -Dfile="$project_folder/data/LondonStations.csv" -Dfrom=2019-01 -Dto=2019-05
-  java -jar "$project_folder/target/crime.jar" -Dapi=stop-and-search-by-force -Dfrom=2019-01 -Dto=2019-05
+  if [[ "$selectedApi" == "all" ]]; then
+    for i in "${api[@]}"; do
+      java -jar "$project_folder/target/crime.jar" -Dapi="$i" -Dfile="$project_folder/data/LondonStations.csv" -Dfrom=2019-01 -Dto=2019-05
+    done
+  elif [[ $selectedApi -eq 0 ]]; then
+    return 0
+  else
+    java -jar "$project_folder/target/crime.jar" -Dapi="${api[selectedApi]}" -Dfile="$project_folder/data/LondonStations.csv" -Dfrom=2019-01 -Dto=2019-05
+  fi
 }
 
-anazyle() {
-  java -jar "$project_folder/target/crime.jar" -Dapi="$1" -Drows="$2" -Dfile="$project_folder/sql/queries/"
+analyze() {
+  if [[ $anazyleQuery -eq 0 ]]; then
+    return 0;
+  fi
+  java -jar "$project_folder/target/crime.jar" -Dapi="$anazyleQuery" -Drows="$rows" -Dfile="$project_folder/sql/queries/"
 }
 
 parse_arguments() {
-  while getopts DBvhr:Q:f: opt "$@"; do
+  while getopts dbvhr:q:f:w: opt "$@"; do
   case $opt in
+    f)
+    project_folder="$OPTARG"
+    ;;
     v)
-    quiet="-"
+    quiet=" "
     ;;
     h)
     help
     exit 0
     ;;
-    f)
-    project_folder=$OPTARG
+    w)
+    selectedApi="$OPTARG"
     ;;
-    D)
+    d)
+    check_postgres
     drop_table_data
-    exit 0
     ;;
-    B)
-    build_project "$project_folder"
+    b)
+    build_project
     ;;
     r)
     rows=$OPTARG
     ;;
-    Q)
-    anazyle "$OPTARG" "$rows"
-    exit 0;
+    q)
+    anazyleQuery="$OPTARG"
     ;;
     *)
     echo "Unknown argument."
-    exit 0
+    help
+    exit 1
     ;;
   esac
   done
@@ -130,3 +144,4 @@ parse_arguments "$@"
 check_postgres
 create_tables_if_not_exists
 run_application
+analyze
